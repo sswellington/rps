44efb0517d57f45b1049e6afed571263fe12b4fb
sswellington <wellws@pm.me>
2021-01-07 11:37:39 -0300

    refactor: correlation in scatter plot

--- src/visualize/scatter.py
+++ src/visualize/scatter.py
@@ -4,20 +4,20 @@ import matplotlib.pyplot as plt
 
 PATH = 'database/preprocessing.csv'
 
+
 if __name__ == "__main__" :
-    label = ('V','O','I','P')
-    header = ('Vazão', 'Temperatura', 'pH', 'Condutividade', 'OD',
+    HEADER = ('Vazão', 'Temperatura', 'pH', 'Condutividade', 'OD',
               'HCO3', 'MPS','Clorofila', 'COD',
               'NO', 'NO.1', 'NH', 'NI', 'N', 'NT', 'NOD',
               'NP', 'PO4', 'PTD', 'PT', 'PO', 'PP', 'Silica', 
               'Cl', 'SO4', 'Na', 'Ca', 'K', 'Mg')
 
-    len_header = len(header)
+    len_header = len(HEADER)
     df = pd.read_csv(PATH)
     
     for i in range(len_header):
         for j in range(len_header):
-            df.plot.scatter(x=header[i],y=header[j], c='DarkBlue')
-            plt.savefig('view/scatter/'+header[i]+'-vs-'+header[j]+'.pdf', dpi=300) 
+            df.plot.scatter(x = HEADER[i], y = HEADER[j], c='DarkBlue')
+            plt.savefig('view/scatter/' + HEADER[i] + '-vs-' + HEADER[j] +'.pdf', dpi=300) 
             plt.cla()   # Clear axis
             plt.clf()   # Clear figure
