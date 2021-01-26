82f8f594cf3194eb0ff4377037a9b5510bfc1206
sswellington <wellws@pm.me>
2021-01-26 17:42:19 -0300

    refactor: exception and logging

--- src/preprocessing/main.py
+++ src/preprocessing/main.py
@@ -1,18 +1,28 @@
+import logging
 from pandas import read_csv as pd_read_csv
 
 from library import Preprocessing
 
 
 if __name__ == "__main__" :
+    logging.basicConfig(filename=('log/preprocessing.txt'),
+                level=logging.ERROR, 
+                format=' %(asctime)s - %(levelname)s - %(message)s')
+    
     PATH = 'database/source.csv'
     LABEL = ('V','O','I','P')
-    STARTING_YEAR = 1995
-    FINAL_YEAR = 2019
     
-    df = pd_read_csv(PATH)
+    try: 
+        df = pd_read_csv(PATH)
+    except:
+        logging.error('pandas did not find the dataset source')
+    finally:
+        del PATH
+        
     rps = Preprocessing(df)
     del df
     
     rps.export(LABEL, 'database/preprocessing.csv')
     del rps
+    del LABEL
     
